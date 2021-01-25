d7948e166f9f157818e0a4e0d217012f7944183b
johnsony-freshworks <johnson.yesubalan@freshworks.com>
2021-01-25 17:21:29 +0530

    + Comment DB

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20210125113708) do
+ActiveRecord::Schema.define(version: 20210125114838) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "title",       limit: 255
     t.text     "description", limit: 65535
     t.datetime "created_at",                null: false
     t.datetime "updated_at",                null: false
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.text     "comment",    limit: 65535
+    t.datetime "created_at",               null: false
+    t.datetime "updated_at",               null: false
   end
 
   create_table "events", force: :cascade do |t|
